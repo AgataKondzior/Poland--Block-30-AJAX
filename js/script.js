c7f266c01f153ac9c2309cec938df42d7d3bbb4a
Justyna <justynazarudzka@gmail.com>
2017-05-26 12:20:41 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function loadData() {
     var cityStr = $("#city").val();
     var address = streetStr + ", " + cityStr;
     $greeting.text('So you want to live at ' + address + '?');
-    var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
+    var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $("body").append('<img class="bgimg" src="'+streetViewURL+'">');
 	
     
@@ -25,16 +25,39 @@ function loadData() {
 	 //NY times
     var nyTimesUrl="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=9634e5f5718e462e9df187b75ed7a067";
 
-$.getJSON( nyTimesUrl, function( data ) {
-	$nytHeaderElem.text=('New York Times Article About' + cityStr);
-	 articles=data.response;
-    for(var i=0;i<articles.length;i++){
-        var articleData=articles[i];
-        $nytElem.append('<li class="article">'+'<a href=" '+ articleData.web_url +' ">'+articleData.headline.main+'</a>'+'<p>'+articleData.snippet+'</p>' +'</li>');
-    };
-});
+	$.getJSON(nyTimesUrl, function(data){
+		console.log(data);
+		$nytHeaderElem.text('New York Times article about ' + cityStr);
+		articles = data.response.docs;
+		for(var i = 0; i < articles.length; i++){
+			var article = articles[i];
+			$nytElem.append('<a class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a' + '<p>' + article.snippet + '</p>'
+			+'</li>');
+		}
+	})
+	
+	//wiki
+	
+	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=wikiCallback&search="+cityStr;
 
 
+	var wikiRequestTimeout = setTimeout(function(){
+		$wikiElem.text("failed to get wikipedia resources");
+	}, 8000);
+
+	$.ajax({
+		url: wikiUrl,
+		dataType: "jsonp",
+		success: function(response){
+			var articleList = response[1];
+			for(var i = 0; i<articleList.length; i++){
+				articleStr = articleList[i];
+				var urls = 'http://en.wikipedia.org/wiki/'+articleStr;
+				$wikiElem.append('<li><a href="'+wikiUrl+'">'+'</a></li>');
+			};
+		}
+
+	});
 return false
 };
 
