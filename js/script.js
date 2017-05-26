c16fde6fc596f796cfe79a0060c246cd84f4fd95
Justyna <justynazarudzka@gmail.com>
2017-05-26 19:55:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-
-function loadData() {
-
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -9,18 +6,17 @@ function loadData() {
 
     $wikiElem.text("");
     $nytElem.text("");
+    
 	
-	//streetview
+function loadData() {
 
+	//streetview
     var streetStr = $("#street").val();
     var cityStr = $("#city").val();
     var address = streetStr + ", " + cityStr;
     $greeting.text('So you want to live at ' + address + '?');
     var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $("body").append('<img class="bgimg" src="'+streetViewURL+'">');
-	
-    
-	
 	
 	 //NY times
     var nyTimesUrl="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=9634e5f5718e462e9df187b75ed7a067";
@@ -31,38 +27,34 @@ function loadData() {
 		articles = data.response.docs;
 		for(var i = 0; i < articles.length; i++){
 			var article = articles[i];
-			$nytElem.append('<a class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a' + '<p>' + article.snippet + '</p>'
+			$nytElem.append('<a class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>' + '<p>' + article.snippet + '</p>'
 			+'</li>');
 		}
-	})
-	
+	});
+
 	//wiki
 	
 	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=wikiCallback&search="+cityStr;
 
-
-	var wikiRequestTimeout = setTimeout(function(){
-		$wikiElem.text("failed to get wikipedia resources");
-	}, 8000);
-
 	$.ajax({
-		url: wikiUrl,
-		dataType: "jsonp",
-		success: function(response){
-			var articleList = response[1];
-			for(var i = 0; i<articleList.length; i++){
-				articleStr = articleList[i];
-				var urls = 'http://en.wikipedia.org/wiki/'+articleStr;
-				$wikiElem.append('<li><a href="'+wikiUrl+'">'+'</a></li>');
-			};
-		}
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function( response ) {
+            var articleList = response[1];
 
-	});
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[1];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>')
+            };
+        }
+    });
 return false
 };
 
 
 $('#form-container').submit(loadData);
+
 
 
 //9634e5f5718e462e9df187b75ed7a067 NYTimes key
