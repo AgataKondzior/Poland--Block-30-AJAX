7c05f14da7aac2f0728a39e3bfefd39ad2d34ac5
Justyna <justynazarudzka@gmail.com>
2017-05-26 20:01:26 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,11 +15,11 @@ function loadData() {
     var cityStr = $("#city").val();
     var address = streetStr + ", " + cityStr;
     $greeting.text('So you want to live at ' + address + '?');
-    var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
+    var streetViewURL = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $("body").append('<img class="bgimg" src="'+streetViewURL+'">');
 	
 	 //NY times
-    var nyTimesUrl="http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=9634e5f5718e462e9df187b75ed7a067";
+    var nyTimesUrl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=9634e5f5718e462e9df187b75ed7a067";
 
 	$.getJSON(nyTimesUrl, function(data){
 		console.log(data);
